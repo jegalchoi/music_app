22ef11e1730ae1866e74a8eb64cd933e05b83098
jegalchoi <thefreshbegal@gmail.com>
2019-08-20 16:35:35 -0700

    Create UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,23 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def new
     @user = User.new
-
     render :new
   end
 
   def create
     @user = User.new(user_params)
 
-    if @user.save!
+    if @user.save
       login!(@user)
       redirect_to user_url(@user)
     else
-      render json: @user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
     end
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:email, :password)
   end
